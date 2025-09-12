f41f37a46d3911d1d93eb071807045cb28e6d362
hayashirice1000 <y.hayashid@gmail.com>
2025-09-12 22:09:19 +0900

    更新

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const button = document.getElementById("clickMe");
-button.addEventListener("click", () => {
-    alert("ボタンが押されました！");
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault(); // フォームのデフォルトの送信を防止
+
+        cosnt username = form.username.value;
+        const password = form.password.value;
+
+        // 簡単なバリデーション
+        if (username === 'admin' && password === '1234') {
+            alert('ログイン成功!');
+            window.location.href = 'firsthtml.html'; // ログイン成功後のリダイレクト先
+        } else {
+            alert('ユーザー名またはパスワードが間違っています。');
+        };
+    });
+
+
+
 });
