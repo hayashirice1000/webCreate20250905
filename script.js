4e996c47b66b5b67107f66bf0f7cd4289b4f07da
hayashirice1000 <y.hayashid@gmail.com>
2025-09-12 22:39:08 +0900

    更新

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.querySelector('form');
-
+//　HTMLが完全に読み込まれた後に実行される
     form.addEventListener('submit', function (event) {
         event.preventDefault(); // フォームのデフォルトの送信を防止
 
-        cosnt username = form.username.value;
+        const username = form.username.value;
         const password = form.password.value;
 
         // 簡単なバリデーション
