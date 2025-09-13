3e3c84b8cbb11377bbc451f5792ad2c719569feb
hayashirice1000 <y.hayashid@gmail.com>
2025-09-13 14:55:55 +0900

    更新

---
+++ flask_login/app.py
@@ -0,0 +1,31 @@
+# Flaskアプリケーションのインスタンスを作成
+from flask import Flask, render_template, request, redirect, url_for
+
+# Flaskアプリケーションのインスタンスを作成
+app = Flask(__name__)
+
+# 学習用の簡易ユーザー情報（本番ではDBで管理）
+USERS = { # ユーザー名とパスワードの辞書    
+    "admin": "1234", 
+    "user1": "abcd"
+}
+# ルートURLに対するルーティング
+@app.route("/", methods=["GET", "POST"]) # GETとPOSTメソッドを許可
+def login():# ログインページの表示と処理
+    error = ""# エラーメッセージの初期化
+    if request.method == "POST":# フォームが送信された場合
+        username = request.form["username"]# フォームからユーザー名を取得
+        password = request.form["password"]# フォームからパスワードを取得
+         # ユーザー名とパスワードの検証
+        if username in USERS and USERS[username] == password:
+            return redirect(url_for("welcome", username=username))
+        else:
+            error = "ユーザー名かパスワードが間違っています。"
+    return render_template("login.html", error=error)
+
+@app.route("/welcome/<username>")
+def welcome(username):
+    return render_template("welcome.html", username=username)
+
+if __name__ == "__main__":
+    app.run(debug=True)
