dc3b970a9e1fb8287c4f94e1935357fca85094e9
hayashirice1000 <y.hayashid@gmail.com>
2025-09-17 14:45:31 +0900

    更新

--- public/script.js
+++ public/script.js
@@ -7,13 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const username = form.username.value;
         const password = form.password.value;
 
-        // 簡単なバリデーション
-        if (username === 'admin' && password === '1234') {
-            alert('ログイン成功!');
-            window.location.href = 'firsthtml.html'; // ログイン成功後のリダイレクト先
-        } else {
-            alert('ユーザー名またはパスワードが間違っています。');
-        };
+        
     });
 
 
